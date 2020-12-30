e996f985c50d10a315a11bb10b6aca24bf021480
Merhawi Ghebre Selassie <merhawi@thirdeye.io>
2020-12-31 00:35:54 +0100

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -10,8 +10,6 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeError() });
   }
-
-  console.error(err);
   
   res.status(400).send({
     errors: [{ message: 'Something went wrong ' }],
